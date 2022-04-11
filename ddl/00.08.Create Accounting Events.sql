365a87df2bbb7bdba286fd6524dd0596bdd4afb3
David Richards <davidlamontrichards@gmail.com>
2022-04-10 22:02:14 -0600

    Add associations

--- ddl/00.08.Create Accounting Events.sql
+++ ddl/00.08.Create Accounting Events.sql
@@ -4,6 +4,7 @@ CREATE TABLE `accounting_events` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `amount_fractional_units` int DEFAULT NULL,
   `currency_id` bigint DEFAULT NULL,
+  `entity_id` bigint DEFAULT NULL,
   `memo` varbinary(1024) DEFAULT NULL,
   `observed_at` datetime(6) DEFAULT NULL,
   `transacted_at` datetime(6) DEFAULT NULL,
@@ -18,7 +19,9 @@ CREATE TABLE `accounting_events` (
   `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_rails_71f74f8a2d` (`currency_id`),
+  KEY `fk_rails_ffe45d27d9` (`entity_id`),
   KEY `fk_rails_b849b0dcde` (`accounting_type_id`),
   CONSTRAINT `fk_rails_71f74f8a2d` FOREIGN KEY (`currency_id`) REFERENCES `currencies` (`id`),
-  CONSTRAINT `fk_rails_b849b0dcde` FOREIGN KEY (`accounting_type_id`) REFERENCES `accounting_types` (`id`)
+  CONSTRAINT `fk_rails_b849b0dcde` FOREIGN KEY (`accounting_type_id`) REFERENCES `accounting_types` (`id`),
+  CONSTRAINT `fk_rails_ffe45d27d9` FOREIGN KEY (`entity_id`) REFERENCES `entities` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- ddl/00.10.Create Measures.sql
+++ ddl/00.10.Create Measures.sql
@@ -5,11 +5,14 @@ CREATE TABLE `measures` (
   `name` varchar(255) DEFAULT NULL,
   `description` varbinary(1024) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
+  `periodicity_id` bigint DEFAULT NULL,
   `conversion` varchar(255) DEFAULT NULL,
   `meta_data` varbinary(1024) DEFAULT NULL,
   `version` varchar(255) DEFAULT '0.0.1',
   `status` varchar(255) DEFAULT 'active',
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_rails_b7b4acaf86` (`periodicity_id`),
+  CONSTRAINT `fk_rails_b7b4acaf86` FOREIGN KEY (`periodicity_id`) REFERENCES `periodicities` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
